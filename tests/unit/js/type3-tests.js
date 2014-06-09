fd82b0d93aeaec35880175ab52caffcce03a183b
customcommander <hello@spinjs.com>
2014-06-09 10:26:43 +0100

    add iframe helpers and refactor tests to use them

--- tests/unit/js/type3-tests.js
+++ tests/unit/js/type3-tests.js
@@ -2,6 +2,42 @@ YUI.add('type3-tests', function (Y) {
 
 var suite = new Y.Test.Suite('type3 main tests suite');
 
+/**
+ * Drops an iframe on a page and resolves after 100 ms.
+ *
+ * @param src {String} The url of the iframe
+ * @return {Promise} Resolves after 100ms and passes a reference to the iframe to the callback.
+ */
+function dropIframe(src) {
+
+    var ifrm_name = Y.guid('ifrm');
+
+    Y.one('body').append(
+        Y.Lang.sub('<iframe src="{src}" name="{name}"></iframe>', {
+            src: src,
+            name: ifrm_name
+        })
+    );
+
+    return new Y.Promise(function (resolve) {
+        Y.later(100, null, function () {
+            var i = 0, ifrm;
+            do {
+                ifrm = window.frames[i];
+                i++;
+            } while ( ifrm.name !== ifrm_name && i<window.frames.length );
+            resolve(ifrm);
+        });
+    });
+}
+
+/**
+ * Removes all iframes on the page.
+ */
+function removeIframes() {
+    Y.all('iframe').remove();
+}
+
 suite.add(new Y.Test.Case({
 
     name: 'public exposure',
@@ -51,18 +87,25 @@ suite.add(new Y.Test.Case({
     name: '.texts()',
 
     init: function () {
-        Y.one('body').append('<iframe src="assets/search1.html"></iframe>');
+        removeIframes();
     },
 
     destroy: function () {
-        Y.all('iframe').remove();
+        removeIframes();
     },
 
     'should return an array of matching texts': function () {
-        this.wait(function () {
-            var texts = window.frames[0].type3('foo').texts();
-            Y.ArrayAssert.itemsAreSame(texts, [ 'foo_1', 'foo_2', 'foo_3', 'foo_4' ]);
-        }, 100);
+
+        var test = this;
+
+        dropIframe('assets/search1.html').then(function (frm) {
+            test.resume(function () {
+                var texts = frm.type3('foo').texts();
+                Y.ArrayAssert.itemsAreSame(texts, [ 'foo_1', 'foo_2', 'foo_3', 'foo_4' ]);
+            });
+        });
+
+        this.wait();
     }
 }));
 
@@ -71,34 +114,44 @@ suite.add(new Y.Test.Case({
     name: '.nodes()',
 
     init: function () {
-        Y.all('iframe').remove();
-    },
-
-    setUp: function () {
-        Y.one('body').append('<iframe src="assets/nodes-tests.html"></iframe>');
+        removeIframes();
     },
 
     tearDown: function () {
-        Y.all('iframe').remove();
+        removeIframes();
     },
 
     'should return an array of matching text nodes': function () {
-        this.wait(function () {
-            var nodes = window.frames[0].type3('foo').nodes();
-            Y.ArrayAssert.itemsAreSame(nodes, [
-                window.frames[0].document.getElementById('txt1').firstChild,
-                window.frames[0].document.getElementById('txt2').firstChild,
-                window.frames[0].document.getElementById('txt3').firstChild,
-                window.frames[0].document.getElementById('txt4').firstChild,
-                window.frames[0].document.getElementById('txt5').firstChild
-            ]);
-        }, 100);
+
+        var test = this;
+
+        dropIframe('assets/nodes-tests.html').then(function (frm) {
+            test.resume(function () {
+                var nodes = frm.type3('foo').nodes();
+                Y.ArrayAssert.itemsAreSame(nodes, [
+                    frm.document.getElementById('txt1').firstChild,
+                    frm.document.getElementById('txt2').firstChild,
+                    frm.document.getElementById('txt3').firstChild,
+                    frm.document.getElementById('txt4').firstChild,
+                    frm.document.getElementById('txt5').firstChild
+                ]);
+            });
+        });
+
+        this.wait();
     },
 
     'should return null if there is no match': function () {
-        this.wait(function () {
-            Y.Assert.isNull(window.frames[0].type3('unknown').nodes());
-        }, 100);
+
+        var test = this;
+
+        dropIframe('assets/nodes-tests.html').then(function (frm) {
+            test.resume(function () {
+                Y.Assert.isNull( frm.type3('unknown').nodes() );
+            });
+        });
+
+        this.wait();
     }
 }));
 
@@ -106,25 +159,12 @@ suite.add(new Y.Test.Case({
 
     name: '.wrap(wrapper)',
 
-    init: function () {
-        this.removeIframe();
+    setUp: function () {
+        removeIframes();
     },
 
     tearDown: function () {
-        this.removeIframe();
-    },
-
-    dropIframe: function () {
-        Y.one('body').append('<iframe src="assets/wrap-tests.html"></iframe>');
-        return new Y.Promise(function (resolve) {
-            Y.later(150, null, function () {
-                resolve(window.frames[0]);
-            });
-        });
-    },
-
-    removeIframe: function () {
-        Y.all('iframe').remove();
+        removeIframes();
     },
 
     'should throw if wrapper is not a string': function () {
@@ -137,7 +177,7 @@ suite.add(new Y.Test.Case({
 
         var test = this;
 
-        this.dropIframe().then(function (frm) {
+        dropIframe('assets/wrap-tests.html').then(function (frm) {
             test.resume(function () {
                 frm.type3('bar').wrap('<strong id="wrapped1">{text}</strong>');
                 Y.Assert.isNotNull( Y.one(frm.document).one('#wrapped1') );
